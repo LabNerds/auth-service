7ca9a3ebba707cf878d3e375f7ec34fa7421afcc
Louis <huynguyennbk@gmail.com>
2021-10-19 22:33:01 +0700

    setup: unit test

---
+++ src/example/example.controller.spec.ts
@@ -0,0 +1,37 @@
+import { toPromise } from '@shared/utils';
+import { Repository } from 'typeorm';
+import { ExampleListDto } from './dto/example.list.dto';
+import { ExampleEntity } from './entity/example.entity';
+import { ExampleController } from './example.controller';
+import { ExampleService } from './example.service';
+
+describe('CatsController', () => {
+  let exampleController: ExampleController;
+  let exampleService: ExampleService;
+
+  beforeEach(() => {
+    const exampleRepo: Repository<ExampleEntity> = new Repository();
+    exampleService = new ExampleService(exampleRepo);
+    exampleController = new ExampleController(exampleService);
+  });
+
+  describe('findAll', () => {
+    it('should return an array of cats', async () => {
+      const result: ExampleListDto = {
+        records: [
+          {
+            id: '1',
+            name: 'name',
+            description: 'description',
+          },
+        ],
+      };
+
+      jest
+        .spyOn(exampleService, 'getAllExample')
+        .mockImplementation(() => toPromise(result));
+
+      expect(await exampleController.findAll()).toBe(result);
+    });
+  });
+});
